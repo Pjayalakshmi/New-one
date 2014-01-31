6976861f86d4536595e61cae9e8ac7d727215a98
Pjayalakshmi <Jayalakshmi.cse18@gmail.com>
2014-01-31 18:15:46 +0530

    Create datastruct.py

---
+++ datastruct.py
@@ -0,0 +1,14 @@
+Stack:
+stack=[1,2,3]
+stack.append(4)
+stack
+stack.pop()
+Queue:
+queue=deque(["jkl","mno","pqr"])
+queue.append("stu")
+queue.popleft()
+queue
+Tuples:
+t=1357,2468,'jaya!'
+t[1]
+t
